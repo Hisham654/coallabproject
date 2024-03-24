0cfe7dbab1414e0a84ef2d872bc168f62e5543fa
Zarro <Zarro@DESKTOP-P7O55N6>
2024-03-24 22:42:57 +0500

    Add project files.

---
+++ coallabproject/Models/MAJUContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace coallabproject.Models
+{
+    public partial class MAJUContext : DbContext
+    {
+
+        public MAJUContext()
+        {
+        }
+
+        public MAJUContext(DbContextOptions<MAJUContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Student> Students { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-P7O55N6\\SQLEXPRESS;Database=MAJU;Trusted_Connection=True;trustServerCertificate=true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Age).HasColumnName("AGE");
+
+                entity.Property(e => e.Contact)
+                    .HasMaxLength(200)
+                    .IsUnicode(false)
+                    .HasColumnName("CONTACT");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("NAME");
+
+                entity.Property(e => e.Semester)
+                    .HasMaxLength(200)
+                    .IsUnicode(false)
+                    .HasColumnName("SEMESTER");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ coallabproject/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace coallabproject.Models
+{
+    public partial class Student
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Semester { get; set; }
+        public int? Age { get; set; }
+        public string? Contact { get; set; }
+    }
+}
